15bc66cefb48293144056a9170a67b7a2a2d9fd9
Georg Veichtlbauer <georg@vware.at>
2023-04-17 14:00:59 +0200

    sdm660-common: overlay: Split out Lineage settings
    Change-Id: I1ac3838375e6d1b2e13c3fcaeea684c76b417135

--- common.mk
+++ common.mk
@@ -18,7 +18,8 @@ $(call inherit-product, vendor/nokia/sdm660-common/sdm660-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
